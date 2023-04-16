94bf2313cef690fccbeeae606b89399796199c51
naterattner <naterattner@gmail.com>
2023-04-16 19:25:56 -0400

    working data load

--- src/app.js
+++ src/app.js
@@ -1,3 +1,77 @@
 import { scaleBand, ScaleBand } from "d3";
+import { csv } from "d3";
+import { dsv } from "d3";
+// import * as d3 from "d3";
+import { json } from "d3";
+
 
 console.log('hello test')
+
+/* CONSTANTS AND GLOBALS */
+// const width = ,
+//   height = ,
+//   margin = ,
+//   radius = ;
+
+
+// these variables allow us to access anything we manipulate in init() but need access to in draw().
+// All these variables are empty before we assign something to them.
+// let svg;
+// let xScale;
+// let yScale; ...
+
+/* APPLICATION STATE */
+let state = {
+	data: [],
+	selection: "All", // + YOUR FILTER SELECTION
+  };
+  
+  /* LOAD DATA */
+  // + SET YOUR DATA PATH
+import('../data/20230326_quintiles_player_bins.json').then(data => {
+    console.log("loaded data:", data);
+    state.data = data;
+    init();
+  })
+
+/* INITIALIZING FUNCTION */
+// this will be run *one time* when the data finishes loading in
+function init() {
+	// + SCALES
+  
+  
+	// + AXES
+  
+  
+	// + UI ELEMENT SETUP
+  
+  
+	// + CREATE SVG ELEMENT
+	
+  
+	// + CALL AXES
+  
+  
+	draw(); // calls the draw function
+  }
+  
+  /* DRAW FUNCTION */
+  // we call this every time there is an update to the data/state
+  function draw() {
+	// + FILTER DATA BASED ON STATE
+	const filteredData = state.data
+	  // .filter(d => d.country === state.selection)
+  
+	// + UPDATE SCALE(S), if needed
+	
+  
+	// + UPDATE AXIS/AXES, if needed
+  
+  
+	// UPDATE LINE GENERATOR FUNCTION
+  
+  
+	// + DRAW LINE AND/OR AREA
+	
+  
+  }
