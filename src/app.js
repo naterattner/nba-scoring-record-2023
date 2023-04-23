4b8bb8d80cdc839fcf37034f7be1ba3cbdf93c2d
naterattner <naterattner@gmail.com>
2023-04-23 16:01:15 -0400

    scrollama template

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import { scaleBand, ScaleBand } from "d3";
-// import * as d3 from "d3";
+import * as d3 from "d3";
 // import { json } from "d3";
 import scrollama from "scrollama"; // or...
 
 console.log('hello test')
-console.log(scrollama)
+
 
 /* CONSTANTS AND GLOBALS */
 // const width = ,
@@ -18,6 +18,15 @@ console.log(scrollama)
 // let svg;
 // let xScale;
 // let yScale; ...
+
+// these are variables set up for use in the scrolly
+const main = d3.select("main");
+const scrolly = main.select("#scrolly");
+const figure = scrolly.select("figure");
+const article = scrolly.select("article");
+const step = article.selectAll(".step");
+console.log(step)
+const scroller = scrollama(); // initialize the scrollama
 
 /* APPLICATION STATE */
 let state = {
@@ -36,6 +45,22 @@ import('../data/20230326_quintiles_player_bins.json').then(data => {
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in
 function init() {
+	// SCROLLAMA
+	// 1. force a resize on load to ensure proper dimensions are sent to scrollama
+	handleResize();
+
+	// 2. setup the scroller passing options
+	// 		this will also initialize trigger observations
+	// 3. bind scrollama event handlers (this can be chained like below)
+	scroller
+		.setup({
+			step: "#scrolly article .step",
+			offset: 0.33,
+			debug: true
+		})
+		.onStepEnter(handleStepEnter);
+
+
 	// + SCALES
   
   
@@ -74,3 +99,36 @@ function init() {
 	
   
   }
+
+/* SCROLL INTERACTIONS */
+
+
+// generic window resize listener event
+function handleResize() {
+	// 1. update height of step elements
+	const stepH = Math.floor(window.innerHeight * 0.75);
+	step.style("height", stepH + "px");
+
+	const figureHeight = window.innerHeight / 2;
+	const figureMarginTop = (window.innerHeight - figureHeight) / 2;
+
+	figure
+		.style("height", figureHeight + "px")
+		.style("top", figureMarginTop + "px");
+
+	// 3. tell scrollama to update new element dimensions
+	scroller.resize();
+}
+
+function handleStepEnter(response) {
+	console.log(response);
+	// response = { element, direction, index }
+
+	// add color to current step only
+	step.classed("is-active", function (d, i) {
+		return i === response.index;
+	});
+
+	// update graphic based on step
+	figure.select("p").text(response.index + 1);
+}
